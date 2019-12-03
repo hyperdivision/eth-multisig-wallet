df6767aa40c903932878c85e0c9164228342a6af
Emil Bay <github@tixz.dk>
2019-12-03 16:14:39 +0100

    Fix create2 call

--- contracts/CloneFactory.sol
+++ contracts/CloneFactory.sol
@@ -32,7 +32,7 @@ contract CloneFactory {
             mstore(clone, 0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000)
             mstore(add(clone, 0x14), targetBytes)
             mstore(add(clone, 0x28), 0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000)
-            addr := create2(0, salt, clone, 0x37)
+            addr := create2(0, clone, 0x37, salt)
             if iszero(extcodesize(addr)) {
               revert(0, 0)
             }
