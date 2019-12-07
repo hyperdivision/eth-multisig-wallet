801fa0f9e5f9602ec36d5969c60df507c77faef3
Emil Bay <github@tixz.dk>
2019-12-07 14:59:40 +0100

    Avoid event shadowing class

--- contracts/Deposit.sol
+++ contracts/Deposit.sol
@@ -3,7 +3,7 @@ pragma solidity 0.5.12;
 import "./IERC20.sol";
 
 contract Deposit {
-    event Deposit(address from, uint amount);
+    event DepositForwarded(address from, uint amount);
     address payable public trustedOwner;
 
     constructor (address payable _trustedOwner) public {
@@ -21,9 +21,9 @@ contract Deposit {
         payable
     {
         require(msg.data.length == 0, "Wallet: fallback function does not take arguments");
-        (bool success, ) = trustedOwner.call.value(msg.value)();
+        (bool success, ) = trustedOwner.call.value(msg.value)("");
         require(success);
-        emit Deposit(msg.sender, msg.value);
+        emit DepositForwarded(msg.sender, msg.value);
     }
 
     function sweepERC20(address ERC20Address) external {
