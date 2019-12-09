a8a3fe88ee509a5f891ccbae551c0878e712a1bd
Emil Bay <github@tixz.dk>
2019-12-09 16:49:35 +0100

    Set compiler version in truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -7,5 +7,17 @@ module.exports = {
       port: 7545,
       network_id: '*'
     }
+  },
+  compilers: {
+    solc: {
+      version: '0.5.12',
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 2000
+        },
+        evmVersion: 'petersburg'
+      }
+    }
   }
 }
