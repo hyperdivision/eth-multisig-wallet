5d8c189ee10e63d587cc3e1586494ff00b53be32
Emil Bay <github@tixz.dk>
2019-12-10 15:28:36 +0100

    Add tests with erc20 contracts

--- tests/truffle/deposit.js
+++ tests/truffle/deposit.js
@@ -2,6 +2,9 @@
 
 const Deposit = artifacts.require('Deposit')
 const DepositFactory = artifacts.require('DepositFactory')
+const GoodERC20 = artifacts.require('tests/GoodERC20')
+const BadERC20 = artifacts.require('tests/BadERC20')
+const GasMiningERC20 = artifacts.require('tests/GasMiningERC20')
 
 const crypto = require('crypto')
 const depositAddress = require('../../lib/deposit-address')
@@ -105,6 +108,100 @@ contract('2nd Auth test', async accounts => {
     assert(BigInt(initialBalance) - BigInt(await web3.eth.getBalance(await instance.trustedOwner.call())) < 15n * EPSILON, 'account balance after sweep')
   })
 
+  it('test good erc20 sweep', async () => {
+    const instance = await Deposit.new(accounts[0])
+    const factory = await DepositFactory.new(instance.address)
+
+    var salt = crypto.randomBytes(32)
+
+    const forwardAddr = toAddress(
+      depositAddress(
+        fromAddress(factory.address),
+        cloneFactory(fromAddress(instance.address)),
+        salt
+      )
+    )
+
+    const erc20 = await GoodERC20.new()
+    assert((await erc20.balanceOf(forwardAddr)).toString() === '0', 'initial erc20 balance')
+    await erc20.transferFrom(erc20.address, forwardAddr, '1000')
+    assert((await erc20.balanceOf(forwardAddr)).toString() === '1000', 'transferred erc20 balance')
+    assert((await erc20.balanceOf(await instance.trustedOwner.call())).toString() === '0', 'transferred erc20 balance')
+
+    await factory.create(await instance.trustedOwner.call(), salt)
+    const specificInstance = await Deposit.at(forwardAddr)
+    assert((await erc20.balanceOf(forwardAddr)).toString() === '1000', 'transferred erc20 balance')
+    await specificInstance.sweepERC20(erc20.address, '50000')
+    assert((await erc20.balanceOf(forwardAddr)).toString() === '0', 'transferred erc20 balance')
+    assert((await erc20.balanceOf(await instance.trustedOwner.call())).toString() === '1000', 'transferred erc20 balance')
+  })
+
+  it('test bad erc20 sweep', async () => {
+    const erc20 = await BadERC20.new()
+    const instance = await Deposit.new(accounts[0])
+    const factory = await DepositFactory.new(instance.address)
+
+    var salt = crypto.randomBytes(32)
+
+    const forwardAddr = toAddress(
+      depositAddress(
+        fromAddress(factory.address),
+        cloneFactory(fromAddress(instance.address)),
+        salt
+      )
+    )
+
+    assert((await erc20.balanceOf(forwardAddr)).toString() === '0', 'initial erc20 balance')
+    await erc20.transferFrom(erc20.address, forwardAddr, '1000')
+    assert((await erc20.balanceOf(forwardAddr)).toString() === '1000', 'transferred erc20 balance')
+    assert((await erc20.balanceOf(await instance.trustedOwner.call())).toString() === '0', 'transferred erc20 balance')
+
+    await factory.create(await instance.trustedOwner.call(), salt)
+    const specificInstance = await Deposit.at(forwardAddr)
+    assert((await erc20.balanceOf(forwardAddr)).toString() === '1000', 'transferred erc20 balance')
+    try {
+      await specificInstance.sweepERC20(erc20.address, '50000')
+    } catch (ex) {
+      assert(true, 'failed to execute')
+      assert((await erc20.balanceOf(forwardAddr)).toString() === '1000', 'transferred erc20 balance')
+      assert((await erc20.balanceOf(await instance.trustedOwner.call())).toString() === '0', 'transferred erc20 balance')
+    }
+  })
+
+  it('test gas mining erc20 sweep', async () => {
+    const erc20 = await GasMiningERC20.new()
+    const instance = await Deposit.new(accounts[0])
+    const factory = await DepositFactory.new(instance.address)
+
+    var salt = crypto.randomBytes(32)
+
+    const forwardAddr = toAddress(
+      depositAddress(
+        fromAddress(factory.address),
+        cloneFactory(fromAddress(instance.address)),
+        salt
+      )
+    )
+
+    assert((await erc20.balanceOf(forwardAddr)).toString() === '0', 'initial erc20 balance')
+    await erc20.transferFrom(erc20.address, forwardAddr, '1000')
+    assert((await erc20.balanceOf(forwardAddr)).toString() === '1000', 'transferred erc20 balance')
+    assert((await erc20.balanceOf(await instance.trustedOwner.call())).toString() === '0', 'transferred erc20 balance')
+
+    await factory.create(await instance.trustedOwner.call(), salt)
+    const specificInstance = await Deposit.at(forwardAddr)
+    assert((await erc20.balanceOf(forwardAddr)).toString() === '1000', 'transferred erc20 balance')
+    const initialBalance = await web3.eth.getBalance(accounts[0])
+
+    try {
+      console.log(await specificInstance.sweepERC20(erc20.address, '5000000'))
+    } catch (ex) {
+      assert(BigInt(initialBalance) - BigInt(await web3.eth.getBalance(accounts[0])) < BigInt(await web3.eth.getGasPrice()) * (5000000n + 42671n))
+      assert(true, 'failed to execute')
+      assert((await erc20.balanceOf(forwardAddr)).toString() === '1000', 'transferred erc20 balance')
+      assert((await erc20.balanceOf(await instance.trustedOwner.call())).toString() === '0', 'transferred erc20 balance')
+    }
+  })
 })
 
 function toAddress (buf) {
