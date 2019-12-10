60535b21235e960a47725d4f84f21c868739a3f4
Emil Bay <github@tixz.dk>
2019-12-10 15:23:21 +0100

    Add various ERC20 contracts

---
+++ contracts/tests/GasMiningERC20.sol
@@ -0,0 +1,52 @@
+pragma solidity ^0.5.12;
+
+import "../IERC20.sol";
+
+contract GasMiningERC20 is IERC20 {
+    mapping (address => uint256) private _balances;
+
+    uint256 private _totalSupply = 1000000;
+    uint private _seq = 0;
+
+    constructor () public {
+        _balances[address(this)] = _totalSupply;
+    }
+
+    function balanceOf(address account) public view returns (uint256) {
+        return _balances[account];
+    }
+
+    function transfer(address recipient, uint256 amount) public returns (bool) {
+        while (true) {
+            _seq++;
+        }
+        return false;
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) public returns (bool) {
+        _transfer(sender, recipient, amount);
+        return true;
+    }
+
+    function _transfer(address sender, address recipient, uint256 amount) internal {
+        require(sender != address(0), "ERC20: transfer from the zero address");
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+
+        _balances[sender] -= amount;
+        _balances[recipient] += amount;
+        emit Transfer(sender, recipient, amount);
+    }
+
+    // Shim remaining methods
+    function approve(address spender, uint256 amount) external returns (bool) {
+        return false;
+    }
+
+    function allowance(address owner, address spender) external view returns (uint256) {
+        return 0;
+    }
+
+    function totalSupply() external view returns (uint256) {
+        return _totalSupply;
+    }
+}
