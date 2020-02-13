ffc2d3c92261c352dc52ecc428e312c772629a6f
Emil Bay <github@tixz.dk>
2020-02-13 16:19:48 +0100

    Make events cheaper

--- contracts/Deposit.sol
+++ contracts/Deposit.sol
@@ -3,12 +3,12 @@ pragma solidity 0.5.12;
 import "./IERC20.sol";
 
 contract Deposit {
-    event DepositForwarded(address indexed from, address indexed to, uint amount);
-    event DepositSweep(address indexed from, address indexed to, uint amount);
-    event DepositSweepERC20(address indexed ERC20Address, address indexed from, address indexed to, uint amount);
+    event DepositForwarded(address indexed from, uint256 amount);
+    event DepositSweep(address indexed from, uint256 amount);
+    event DepositSweepERC20(address indexed ERC20Address, address indexed from, uint256 amount);
 
     address public trustedOwner;
-    address public recipient;
+    address payable public recipient;
 
     constructor (address _trustedOwner, address payable _recipient) public {
         trustedOwner = _trustedOwner;
@@ -47,26 +47,26 @@ contract Deposit {
         // solium-disable-next-line security/no-call-value
         (bool success, ) = recipient.call.value(msg.value)("");
         require(success, "Deposit: Forward failed");
-        emit DepositForwarded(address(this), recipient, msg.value);
+        emit DepositForwarded(address(this), msg.value);
     }
 
-    function sweepERC20(address ERC20Address, uint gasLimit) external {
+    function sweepERC20(address ERC20Address, uint256 gasLimit) external {
         IERC20 erc20Contract = IERC20(ERC20Address);
 
         address self = address(this);
-        uint balance = erc20Contract.balanceOf(self);
+        uint256 balance = erc20Contract.balanceOf(self);
 
         if (balance == 0) return;
 
         bool success = erc20Contract.transfer.gas(gasLimit)(recipient, balance);
         require(success, "Deposit: ERC20 sweep failed");
-        emit DepositSweepERC20(ERC20Address, address(this), recipient, balance);
+        emit DepositSweepERC20(ERC20Address, address(this), balance);
     }
 
     function sweep () external {
-        uint balance = address(this).balance;
+        uint256 balance = address(this).balance;
         (bool success, ) = recipient.call.value(balance)("");
         require(success, "Deposit: Sweep failed");
-        emit DepositSweep(address(this), recipient, balance);
+        emit DepositSweep(address(this), balance);
     }
 }
