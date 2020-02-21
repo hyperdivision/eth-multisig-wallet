f5276ebf1083e39cb602d3b82282ed8a5348f2a8
Emil Bay <github@tixz.dk>
2020-02-21 10:50:02 +0100

    Lock down deposit factory

--- contracts/DepositFactory.sol
+++ contracts/DepositFactory.sol
@@ -7,11 +7,30 @@ contract DepositFactory is CloneFactory {
     event Deployed(address payable indexed at);
     address public templateAddress;
 
-    constructor (address _templateAddress) public {
+    address public trustedOwner;
+    address public recipient;
+
+    constructor (address _templateAddress, address _trustedOwner, address _recipient) public {
         templateAddress = _templateAddress;
+        trustedOwner = _trustedOwner;
+        recipient = _recipient;
     }
 
-    function create(address trustedOwner, address recipient, uint256 salt) public returns(address) {
+    function replaceOwner (address newOwner) external {
+        require(msg.sender == trustedOwner, "Deposit: Only trustedOwner can replace themselves");
+        require(address(0) != newOwner, "Deposit: newOwner cannot be empty");
+
+        trustedOwner = newOwner;
+    }
+
+    function replaceRecipient (address newRecipient) external {
+        require(msg.sender == trustedOwner, "Deposit: Only trustedOwner can replace recipient");
+        require(address(0) != newRecipient, "Deposit: newRecipient cannot be empty");
+
+        recipient = newRecipient;
+    }
+
+    function create(uint256 salt) external returns(address) {
         address payable spawned = super.create2Clone(templateAddress, salt);
         Deposit(spawned).init(trustedOwner, recipient);
         emit Deployed(spawned);
