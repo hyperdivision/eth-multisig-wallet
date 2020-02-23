c3e3616008a5d4e749646375eb2551a4cbd97dfa
Emil Bay <github@tixz.dk>
2020-02-23 15:59:35 +0100

    Export full API from index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-throw new Error('TODO')
+module.exports = {
+  QuorumOwners: require('./lib/quorum-owners-api'),
+  PullWithdrawable: require('./lib/pull-withdrawable-api'),
+  DepositFactory: require('./lib/deposit-factory-api'),
+  Deposit: require('./lib/deposit-api'),
+  keygen: require('./lib/keygen')
+}
